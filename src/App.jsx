2b929515412e51f7a2b217f5c47fcf44c2a3a0b3
believer-15 <ujjwalsinha15@gmail.com>
2024-10-27 16:25:06 +0530

    Added theme in tailwind config like color, font etc

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className='bg-backgroundColor'>Hello</h1>
+      <div className="font-merriweather text-xl">
+        This text uses the Roboto font!
+      </div>
+      <div className="text-4xl font-merriweather font-bold">
+        GALLERY
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: '#8BB5C9',
+      },
+      fontFamily: {
+        merriweather: ['Merriweather', 'Helvetica', 'Arial','sans-serif'],
+      },
+      fontSize: {
+        'xl': '1rem',
+        '4xl': '2.0rem',
+      },
+      fontWeight: {
+        bold: 700,
+      },
+      fontStyle: {
+        normal: 'normal',
+      },
+    },
   },
   plugins: [],
 }
