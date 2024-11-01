01212efc98b58489decf5a4ae2e81f6bef8710a1
believer-15 <ujjwalsinha15@gmail.com>
2024-11-01 19:07:17 +0530

    Added Carousel

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,17 +1,55 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 function Gallery() {
-    return (
-        <>
-            <section className="flex items-center justify-center font-merriweather border-2">
-                <div>
-                    <h1 className="text-xl font-bold tracking-[0.15em]">GALLERY</h1>
-                </div>
-                <div>
-                    
-                </div>
-            </section>
-        </>
-    );
-};
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+        <SwiperSlide>          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
 
 export default Gallery;
