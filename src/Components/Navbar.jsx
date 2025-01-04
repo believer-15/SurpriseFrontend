7d218f4f9f36dc5d70cb7838a40bb0b1825fbf23
Akash Singh <44118858+Akash5523@users.noreply.github.com>
2025-01-04 12:08:54 +0000

    Fixed conflict added active navabar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,85 +1,138 @@
 import { useState } from 'react';
-import Logo from '..//assets/Logo.png';
+import Logo from '../assets/Logo.png';
 import { useScroll } from '../States/State';
-
-// import { NavLink } from 'react-router';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [activeSection, setActiveSection] = useState('home'); // Track the active section
 
     const closeSidebar = () => {
         setIsOpen(!isOpen);
-    }
-    const { scrollToSection } = useScroll();
-    // const handleClick = () => {
-    //     scrollToSection();
-    //     console.log("Hitting Handle Click");
-    // }
-
+    };
     
+    const { scrollToSection } = useScroll();
+
+    const handleSectionClick = (section) => {
+        setActiveSection(section); // Set active section
+        scrollToSection(section); // Scroll to the clicked section
+    };
 
     return (
-        <>
-            <nav className="top-0 left-0 w-full z-[1000] sticky bg-[#8bb5c9]">
-                <div className="sm:mx-auto sm:max-w-6xl flex flex-wrap items-center justify-between h-18 text-[#161d15] font-merriweather">
-                    <div className="block sm:hidden ml-6">
-                        <button onClick={() => setIsOpen(!isOpen)}>
-                            <svg
-                                className="w-8 h-8 cursor-pointer"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                            </svg>
-                        </button>    
-                    </div>
-                    <div className="flex items-center justify-center sm:mr-0 mr-[4.375rem]">
-                        <img src={Logo} alt="Logo" className="w-21 h-20" />
-                        <p className="text-l font-bold tracking-[0.25em]">SURPRISE</p>
-                    </div>
-                    <div className='hidden md:block'>
-                        <ul className='flex gap-20 font-normal text-m tracking-[0.1875em]'>
-                            <li className="">
-                                <button onClick={() => scrollToSection('home')}>Home</button>
-                            </li>
-                            <li className="">
-                                <button onClick={() => scrollToSection('services')}>Services</button>
-                            </li>
-                            <li className="">
-                                <button onClick={() => scrollToSection('gallery')}>Gallery</button>
-                            </li>
-                            <li className="">
-                                <button onClick={() => scrollToSection('contact')}>Contact</button>
-                            </li>
-                        </ul>
-                    </div>  
-
-                    {/* Dropdown menu for small screens */} 
-                    {isOpen && (
-                        <>
-                        <div
-                            className="fixed inset-0 bg-black opacity-50 z-10"
-                            onClick={closeSidebar} // Close sidebar when clicking overlay
+        <nav className="top-0 left-0 w-full z-[1000] sticky">
+            <div className="sm:mx-auto sm:max-w-6xl flex flex-wrap items-center justify-between h-18 text-[#161d15] font-merriweather">
+                <div className="block sm:hidden ml-6">
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                        <svg
+                            className="w-8 h-8 cursor-pointer"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
                         >
-                        </div>
-                        <nav className='absolute w-[100%] top-0 left-0 z-10 bg-white'>
-                            <div className='flex justify-center'>
-                                <img src={Logo} alt="Logo" className="w-21 h-20" />
-                            </div>
-                            <div className='flex flex-col items-center gap-6 font-normal text-[20px] p-7 tracking-[0.08em]'>
-                                <button onClick={() => scrollToSection('home')}>Home</button>
-                                <button onClick={() => scrollToSection('services')}>Services</button>
-                                <button onClick={() => scrollToSection('gallery')}>Gallery</button>
-                                <button onClick={() => scrollToSection('contact')}>Contact</button>
-                            </div>
-                        </nav>
-                        </>
-                    )}
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>    
                 </div>
-            </nav>
-        </>
+                <div className="flex items-center justify-center sm:mr-0 mr-[4.375rem]">
+                    <img src={Logo} alt="Logo" className="w-21 h-20" />
+                    <p className="text-l font-bold tracking-[0.25em]">SURPRISE</p>
+                </div>
+                <div className="hidden md:block">
+                    <ul className="flex gap-20 font-normal text-m tracking-[0.1875em]">
+                        <li>
+                            <button
+                                onClick={() => handleSectionClick('home')}
+                                className={`${
+                                    activeSection === 'home' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Home
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                onClick={() => handleSectionClick('services')}
+                                className={`${
+                                    activeSection === 'services' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Services
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                onClick={() => handleSectionClick('gallery')}
+                                className={`${
+                                    activeSection === 'gallery' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Gallery
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                onClick={() => handleSectionClick('contact')}
+                                className={`${
+                                    activeSection === 'contact' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Contact
+                            </button>
+                        </li>
+                    </ul>
+                </div>  
+
+                {/* Dropdown menu for small screens */} 
+                {isOpen && (
+                    <>
+                    <div
+                        className="fixed inset-0 bg-black opacity-50 z-10"
+                        onClick={closeSidebar} // Close sidebar when clicking overlay
+                    >
+                    </div>
+                    <nav className='absolute w-[100%] top-0 left-0 z-10 bg-white'>
+                        <div className='flex justify-center'>
+                            <img src={Logo} alt="Logo" className="w-21 h-20" />
+                        </div>
+                        <div className='flex flex-col items-center gap-6 font-normal text-[20px] p-7 tracking-[0.08em]'>
+                            <button
+                                onClick={() => handleSectionClick('home')}
+                                className={`${
+                                    activeSection === 'home' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Home
+                            </button>
+                            <button
+                                onClick={() => handleSectionClick('services')}
+                                className={`${
+                                    activeSection === 'services' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Services
+                            </button>
+                            <button
+                                onClick={() => handleSectionClick('gallery')}
+                                className={`${
+                                    activeSection === 'gallery' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Gallery
+                            </button>
+                            <button
+                                onClick={() => handleSectionClick('contact')}
+                                className={`${
+                                    activeSection === 'contact' ? 'font-bold text-[#000000] border-b-[1px] border-[#000000]' : ''
+                                }`}
+                            >
+                                Contact
+                            </button>
+                        </div>
+                    </nav>
+                    </>
+                )}
+            </div>
+        </nav>
     );
 }
 
