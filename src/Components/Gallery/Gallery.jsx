517af5cffb30c0786004b0214561069231aa5a93
believer-15 <ujjwalsinha15@gmail.com>
2025-02-10 18:07:48 +0530

    Refactor the gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,79 +1,66 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import { useScroll } from '../../States/State';
-
-// Import Swiper styles
+import bridal from '../../assets/bridal.png';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import './styles.css';
-
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 
 function Gallery() {
-    const { sectionRef } = useScroll();
+  const { sectionRef } = useScroll();
 
   return (
-    <>
-    <section className='scroll-mt-[6rem] font-merriweather bg-[#8bb5c9] overflow-hidden' 
+    <section 
+      className='scroll-mt-[6rem] font-merriweather bg-[#8bb5c9] overflow-hidden' 
       ref={(el) => (sectionRef.current['gallery'] = el)}
       id='gallery'
     >
       <div className='flex flex-col items-center justify-around sm:pb-12'>
         <div className='mt-5' data-aos="fade-up">
-            <h1 className="text-lg sm:text-xl font-bold tracking-[0.15em]">GALLERY</h1>
+          <h1 className="text-lg sm:text-xl font-bold tracking-[0.15em]">GALLERY</h1>
         </div>
         <Swiper
-        data-aos="fade-up"
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={false}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-      </Swiper>
+          data-aos="fade-up"
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            waitForTransition: true,
+          }}
+          speed={800}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {[
+            bridal,
+            'https://swiperjs.com/demos/images/nature-2.jpg',
+            'https://swiperjs.com/demos/images/nature-3.jpg',
+            'https://swiperjs.com/demos/images/nature-4.jpg',
+            'https://swiperjs.com/demos/images/nature-5.jpg',
+            'https://swiperjs.com/demos/images/nature-6.jpg',
+            'https://swiperjs.com/demos/images/nature-7.jpg',
+            'https://swiperjs.com/demos/images/nature-8.jpg',
+            'https://swiperjs.com/demos/images/nature-9.jpg',
+          ].map((img, index) => (
+            <SwiperSlide key={index}>
+              <img src={img} alt={`Gallery item ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
-      
-    </>
   );
 }
 
